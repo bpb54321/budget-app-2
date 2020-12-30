019bf37a92a3574b9b5cf5399201f1609ec5b4da
Brian Blosser <brian.blosser@wwt.com>
2020-12-30 12:59:23 -0500

    Get only dispatch from the Context.

--- src/SingleTransactionEntry.js
+++ src/SingleTransactionEntry.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { dispatchTransactionsContext } from "./TransactionsProvider";
 import { transactionAdded } from "./transactionsReducer";
-import useTransactions from "./useTransactions";
 
 const SingleTransactionEntry = () => {
   const [payee, setPayee] = useState("");
-  const [, dispatchTransactions] = useTransactions();
+  const dispatchTransactions = useContext(dispatchTransactionsContext);
   return (
     <form
       onSubmit={(event) => {
